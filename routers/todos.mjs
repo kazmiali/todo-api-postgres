a6d4f3995f972f23040bd8a7ae10826569c0de28
kazmiali <ali.smak099@gmail.com>
2023-02-23 01:32:37 +0500

    Added update route function

--- routers/todos.mjs
+++ routers/todos.mjs
@@ -15,10 +15,22 @@ todosRouter.post('/', (req, res) => {
 });
 
 todosRouter.put('/', (req, res) => {
-	const { title } = req.body;
-	const todo = { title };
-	todos.push(todo);
-	res.status(201).json({ message: 'Todo Created.', data: todo });
+	const { id, title } = req.body;
+	console.log('id', id);
+	console.log('title', title);
+	console.log('todos', todos);
+
+	if (!Number(id)) {
+		res.status(500).json({ message: 'ERROR' });
+	}
+
+	let todo = todos.filter(todo => Number(todo.id) === Number(id));
+	if (todo?.length) {
+		todo = todo[0];
+	}
+	console.log('todo', todo);
+	todo.title = title;
+	res.status(200).json({ message: 'Todo Updated.', data: todo });
 });
 
 todosRouter.delete('/', (req, res) => {
